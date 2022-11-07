52420ce8de4c363b8b76d500b7a0c3161a4c8dd4
bobbeekethdy1025 <bobbeekethdy@gmail.com>
2022-11-06 21:27:15 -0600

    fixed bugs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,7 +19,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jateStorage');
   const request = store.put({content});
   const result = await request;
-  console.log('Data has been saved to the database!', result);
+  console.log('🚀 - data saved to the database', result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
